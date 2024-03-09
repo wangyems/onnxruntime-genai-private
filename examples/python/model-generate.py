fb1198322ea2d2a0f61d14266f495a7e424fc9ea
aciddelgado <aciddelgado@microsoft.com>
2024-03-08 17:04:04 -0800

    fix argument defaults

--- examples/python/model-generate.py
+++ examples/python/model-generate.py
@@ -18,8 +18,20 @@ def main(args):
     input_tokens = tokenizer.encode_batch(prompts)
     if args.verbose: print("Prompt(s) encoded")
 
+    search_options = {}
+    if args.max_length is not None:
+        search_options["max_length"] = args.max_length
+    if args.top_p is not None:
+        search_options["top_p"] = args.top_p
+    if args.top_k is not None:
+        search_options["top_k"] = args.top_k
+    if args.temperature is not None:
+        search_options["temperature"] = args.temperature
+    if args.repetition_penalty is not None:
+        search_options["repetition_penalty"] = args.repetition_penalty
+
     params = og.GeneratorParams(model)
-    params.set_search_options({"max_length": args.max_length, "top_p": args.top_p, "top_k": args.top_k, "temperature": args.temperature, "repetition_penalty": args.repetition_penalty})
+    params.set_search_options(search_options)
     params.input_ids = input_tokens
     if args.verbose: print("GeneratorParams created")
 
@@ -43,11 +55,11 @@ if __name__ == "__main__":
     parser.add_argument('-m', '--model', type=str, required=True, help='Onnx model folder path (must contain config.json and model.onnx)')
     parser.add_argument('-ep', '--execution_provider', type=str, choices=['cpu', 'cuda'], required=True, help='Execution provider (device) to use, default is CPU, use CUDA for GPU')
     parser.add_argument('-pr', '--prompts', nargs='*', required=False, help='Input prompts to generate tokens from')
-    parser.add_argument('-l', '--max_length', type=int, default=512, help='Max number of tokens to generate after prompt')
-    parser.add_argument('-p', '--top_p', type=float, default=0.9, help='Top p probability to sample with')
-    parser.add_argument('-k', '--top_k', type=int, default=50, help='Top k tokens to sample from')
-    parser.add_argument('-t', '--temperature', type=float, default=1.0, help='Temperature to sample with')
-    parser.add_argument('-r', '--repetition_penalty', type=float, default=1.0, help='Repetition penalty to sample with')
+    parser.add_argument('-l', '--max_length', type=int, help='Max number of tokens to generate after prompt')
+    parser.add_argument('-p', '--top_p', type=float, help='Top p probability to sample with')
+    parser.add_argument('-k', '--top_k', type=int, help='Top k tokens to sample from')
+    parser.add_argument('-t', '--temperature', type=float, help='Temperature to sample with')
+    parser.add_argument('-r', '--repetition_penalty', type=float, help='Repetition penalty to sample with')
     parser.add_argument('-v', '--verbose', action='store_true', help='Print verbose output')
     args = parser.parse_args()
     main(args)
